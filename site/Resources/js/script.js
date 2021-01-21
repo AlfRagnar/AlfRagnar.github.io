81b6905da9dfc3a8f6afc4044d8875da81e2a4c0
AlfRagnar <alfi427@gmail.com>
2021-01-21 14:42:07 +0100

    Minor changes to scroll speed on Click

--- site/Resources/js/script.js
+++ site/Resources/js/script.js
@@ -17,14 +17,14 @@ $(document).ready(function () {
   $('.js--scroll-to-plans').click(function () {
     $('html, body').animate(
       { scrollTop: $('.js--section-plans').offset().top },
-      1000
+      1
     );
   });
 
   $('.js--scroll-to-start').click(function () {
     $('html, body').animate(
       { scrollTop: $('.js--section-features').offset().top },
-      1000
+      1
     );
   });
 
@@ -45,7 +45,7 @@ $(document).ready(function () {
             {
               scrollTop: target.offset().top,
             },
-            1000
+            1
           );
           return false;
         }

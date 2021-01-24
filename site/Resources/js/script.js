78a52857acb2849b54b751e70cc2cc1a4ba47bd3
AlfRagnar <69853212+Sharrior@users.noreply.github.com>
2021-01-24 17:41:29 +0100

    Fixed mobile nav bar conflicting with browser

--- site/Resources/js/script.js
+++ site/Resources/js/script.js
@@ -3,17 +3,17 @@ $(document).ready(function () {
   $('.js--section-stickyTrigger').waypoint(
     function (direction) {
       if (direction == 'down') {
-        $('nav').addClass('sticky');
-        $('nav').addClass('animated fadeIn');
-        $('navbar').addClass('mobileNavBar');
+        $('nav').addClass('sticky')
+        $('nav').addClass('animated fadeIn')
+        $('navbar').addClass('mobileNavBar')
       } else {
-        $('nav').removeClass('sticky');
-        $('nav').removeClass('animated fadeIn');
-        $('navbar').removeClass('mobileNavBar');
+        $('nav').removeClass('sticky')
+        $('nav').removeClass('animated fadeIn')
+        $('navbar').removeClass('mobileNavBar')
       }
     },
     { offset: '120px;' }
-  );
+  )
 
   /* Navigation scroll */
   $(function () {
@@ -23,10 +23,8 @@ $(document).ready(function () {
           this.pathname.replace(/^\//, '') &&
         location.hostname == this.hostname
       ) {
-        var target = $(this.hash);
-        target = target.length
-          ? target
-          : $('[name=' + this.hash.slice(1) + ']');
+        var target = $(this.hash)
+        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']')
         if (target.length) {
           $('html,body').animate(
             {
@@ -35,93 +33,108 @@ $(document).ready(function () {
             {
               duration: 100,
             }
-          );
-          return false;
+          )
+          return false
         }
       }
-    });
-  });
+    })
+  })
 
   /* Animations on scroll */
 
   $('.js--wp-1').waypoint(
     function (direction) {
-      $('.js--wp-1').addClass('animated fadeIn');
+      $('.js--wp-1').addClass('animated fadeIn')
     },
     {
       offset: '90%',
     }
-  );
+  )
 
   $('.js--wp-2').waypoint(
     function (direction) {
-      $('.js--wp-2').addClass('animated fadeIn');
+      $('.js--wp-2').addClass('animated fadeIn')
     },
     {
       offset: '90%',
     }
-  );
+  )
 
   $('.js--wp-3').waypoint(
     function (direction) {
-      $('.js--wp-3').addClass('animated fadeIn');
+      $('.js--wp-3').addClass('animated fadeIn')
     },
     {
       offset: '90%',
     }
-  );
+  )
 
   $('.js--wp-4').waypoint(
     function (direction) {
-      $('.js--wp-4').addClass('animated fadeIn');
+      $('.js--wp-4').addClass('animated fadeIn')
     },
     {
       offset: '90%',
     }
-  );
+  )
   $('.js--wp-5').waypoint(
     function (direction) {
-      $('.js--wp-5').addClass('animated fadeIn');
+      $('.js--wp-5').addClass('animated fadeIn')
     },
     {
       offset: '90%',
     }
-  );
+  )
   $('.js--wp-6').waypoint(
     function (direction) {
-      $('.js--wp-6').addClass('animated fadeIn');
+      $('.js--wp-6').addClass('animated fadeIn')
     },
     {
       offset: '90%',
     }
-  );
+  )
 
   /* Mobile navigation */
   $('.js--nav-icon').click(function () {
-    var nav = $('.js--main-nav');
-    var icon = $('.js--nav-icon i');
+    var nav = $('.js--main-nav')
+    var icon = $('.js--nav-icon i')
 
-    nav.slideToggle(200);
+    nav.slideToggle(200)
 
     if (icon.hasClass('ion-navicon-round')) {
-      icon.addClass('ion-close-round');
-      icon.removeClass('ion-navicon-round');
+      icon.addClass('ion-close-round')
+      icon.removeClass('ion-navicon-round')
     } else {
-      icon.addClass('ion-navicon-round');
-      icon.removeClass('ion-close-round');
+      icon.addClass('ion-navicon-round')
+      icon.removeClass('ion-close-round')
     }
-  });
-  /* Collapse Navbar on click */
+  })
+  /* Collapse Mobile Navbar on click */
+
   $('.js--main-nav>li>a').click(() => {
-    var navElements = $('.js--main-nav');
-    var icon = $('.js--nav-icon i');
-    navElements.slideToggle(200);
-    if (icon.hasClass('ion-navicon-round')) {
-      icon.addClass('ion-close-round');
-      icon.removeClass('ion-navicon-round');
-    } else {
-      icon.addClass('ion-navicon-round');
-      icon.removeClass('ion-close-round');
+    const targetWidth = 768
+    const windowWidth = window.innerWidth
+    if (windowWidth <= targetWidth) {
+      const navElements = $('.js--main-nav')
+      const icon = $('.js--nav-icon i')
+      navElements.slideToggle(200)
+      if (icon.hasClass('ion-navicon-round')) {
+        icon.addClass('ion-close-round')
+        icon.removeClass('ion-navicon-round')
+      } else {
+        icon.addClass('ion-navicon-round')
+        icon.removeClass('ion-close-round')
+      }
     }
-  });
-});
+  })
+  addEventListener('resize', () => {
+    const icon = $('.js--nav-icon i')
+    if (innerWidth >= 768) {
+      $('.js--main-nav').show()
+    } else if (icon.hasClass('ion-close-round')) {
+      $('.js--main-nav').show()
+    } else {
+      $('.js--main-nav').hide()
+    }
+  })
+})
